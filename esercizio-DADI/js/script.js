09de4437d2e14fd8ce681fb4394145ab3dedbd6d
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-02-24 18:54:07 +0100

    creato elementi html

--- esercizio-DADI/js/script.js
+++ esercizio-DADI/js/script.js
@@ -1,34 +1,55 @@
+let body = document.querySelector('body');
+
 let arrayNumeriDado = [1, 2, 3, 4, 5, 6];
 
+let container = document.createElement('div');
 
+body.append(container);
 
-
-function generateRandomIntegerInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 let numeroRandomUser = generateRandomIntegerInRange(1, 6);
 console.log(numeroRandomUser);
-
-
-function generateRandomIntegerInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 let numeroRandomComputer = generateRandomIntegerInRange(1, 6);
 console.log(numeroRandomComputer);
 
 
+container.innerHTML = `il numero dell utente è ${numeroRandomUser} <br>`;
+container.innerHTML += `il numero del computer è ${numeroRandomComputer} <br>`;
+
+
+
+
+
+
+
+function generateRandomIntegerInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+function generateRandomIntegerInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
 if (numeroRandomUser > numeroRandomComputer) {
 
     console.log('l utente ha vinto');
+    container.innerHTML += 'l utente ha vinto';
+
 
 } else if (numeroRandomUser == numeroRandomComputer) {
 
     console.log('è un pareggio');
+    container.innerHTML += 'è un pareggio';
 
 } else {
 
     console.log('il computer ha vinto')
-
+    container.innerHTML += 'il computer ha vinto';
 }
+
--- js/script.js
+++ js/script.js
@@ -8,6 +8,15 @@ let body = document.querySelector('body');
 
 
 
+emailUser.document.querySelector('#btn');
+console.log(emailUser);
+
+
+
+
+
+
+
 let accettato = false;
 
 for (let indiceArray = 0; indiceArray < arrayEmail.length; indiceArray++) {
@@ -16,15 +25,18 @@ for (let indiceArray = 0; indiceArray < arrayEmail.length; indiceArray++) {
     if (arrayEmail[indiceArray].toLocaleLowerCase() == emailUser.toLocaleLowerCase()) {
 
         accettato = true;
+        console.log(accettato);
     }
 
     if (accettato == true) {
         console.log('la tua email è presente nella lista');
         divEmailPresente.innerHTML = `${emailUser} la tua email è presente nella lista`;
+        console.log(accettato);
 
     } else if (accettato == false) {
         console.log('non sei il benvenuto nel club');
         divEmailPresente.innerHTML = `${emailUser} non sei il benvenuto nel club`;
+        console.log(accettato);
     }
 
 }
