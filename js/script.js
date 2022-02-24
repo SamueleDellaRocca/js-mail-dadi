745c12c3a89d801f2e9e5a5c866a437c522cdfed
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-02-24 19:31:28 +0100

    collegato l evento al btn,ma non l email all input

--- js/script.js
+++ js/script.js
@@ -2,44 +2,49 @@ let arrayEmail = ['henry@gmail', 'gabriel@gmail', 'mauro@gmail', 'totti@gmail', 
 
 let emailUser = prompt('scrivi la tua email');
 
+
 let divEmailPresente = document.createElement('div');
 
 let body = document.querySelector('body');
 
+let btn = document.querySelector('#btn');
 
-
-emailUser.document.querySelector('#btn');
-console.log(emailUser);
-
-
+btn.addEventListener('click', laFunzione);
 
 
 
 
 
-let accettato = false;
-
-for (let indiceArray = 0; indiceArray < arrayEmail.length; indiceArray++) {
 
 
-    if (arrayEmail[indiceArray].toLocaleLowerCase() == emailUser.toLocaleLowerCase()) {
 
-        accettato = true;
-        console.log(accettato);
+function laFunzione() {
+
+    let accettato = false;
+
+    for (let indiceArray = 0; indiceArray < arrayEmail.length; indiceArray++) {
+
+
+        if (arrayEmail[indiceArray].toLocaleLowerCase() == emailUser.toLocaleLowerCase()) {
+
+            accettato = true;
+            console.log(accettato);
+        }
+
+        if (accettato == true) {
+            console.log('la tua email è presente nella lista');
+            divEmailPresente.innerHTML = `${emailUser} la tua email è presente nella lista`;
+            console.log(accettato);
+
+        } else if (accettato == false) {
+            console.log('non sei il benvenuto nel club');
+            divEmailPresente.innerHTML = `${emailUser} non sei il benvenuto nel club`;
+            console.log(accettato);
+        }
+
     }
 
-    if (accettato == true) {
-        console.log('la tua email è presente nella lista');
-        divEmailPresente.innerHTML = `${emailUser} la tua email è presente nella lista`;
-        console.log(accettato);
-
-    } else if (accettato == false) {
-        console.log('non sei il benvenuto nel club');
-        divEmailPresente.innerHTML = `${emailUser} non sei il benvenuto nel club`;
-        console.log(accettato);
-    }
-
-}
+};
 
 
 body.append(divEmailPresente);
